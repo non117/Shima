aecfc398e29bec7c49625d8db6e07b6c29c67583
non <mail@non117.com>
2012-01-16 03:14:56 +0900

    ∩(＞◡＜*)∩

--- src/test.py
+++ src/test.py
@@ -2,7 +2,7 @@
 from shima import Icon
 
 if __name__ == "__main__":
-    command = u"face 11 base dd55ff 778899 dot cheek 2"
+    command = u"face 11 base dd55ff 778899 dot cheek 3"
     icon = Icon()
     if icon.command_perser(command):
         icon.gen_icon()
